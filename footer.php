c9c89dae2e7786e13fd6b8294a280ab9328268d8
t833w2bma <t883w2@bma.biglobe.ne.jp>
2021-02-17 14:17:09 +0900

    カスタムパーツ

--- footer.php
+++ footer.php
@@ -7,6 +7,9 @@
  * @package storefront
  */
 
+
+ //テンプレートパーツをインクルード
+		get_template_part( 'original/main_bottom' ); 
 ?>
 
 		</div><!-- .col-9 -->
---
+++ original/main_bottom.php
@@ -0,0 +1,73 @@
+<?php
+//  echo __FILE__ ;
+
+ $uri = get_theme_file_uri();
+?>
+
+<div class="container">
+  <ul class="row bnr_space clear">
+    <li class="col-6"><a href=""><img src="<?=$uri?>/slider/20210109pc.jpg"></a></li>
+    <li class="col-6"><a href=""><img src="<?=$uri?>/slider/20210116mp.jpg"></a></li>
+    <li class="col-6"><a href=""><img src="<?=$uri?>/slider/20210123sp.jpg"></a></li>
+    <li class="col-6"><a href=""><img src="<?=$uri?>/slider/20210203tshirt.jpg"></a></li>
+    <li class="col-6"><a href=""><img src="<?=$uri?>/slider/20210206op.jpg"></a></li>
+    <li class="col-6"><a href=""><img src="<?=$uri?>/slider/20210212_lss.jpg"></a></li>
+  </ul>
+</div>
+
+
+<div id="shopping-guide">
+<h2>ご利用案内</h2>
+
+<h3>サイズについて</h3>
+<p class="use_info">
+古着のサイズは表記サイズではなく、必ず実寸をご参照ください。表記サイズはメーカー・ブランド・服の作られた時代によって実寸とイメージは異なります。実寸は商品詳細ページの商品説明文の下に記載されております。
+</p><ul class="size_list">
+<li class="li_tops visible">トップス</li>
+<li class="li_bott">ボトムス</li>
+<li class="li_over">オーバーオール</li>
+<li class="li_ponc">ポンチョ</li>
+<li class="li_snea">スニーカー</li>
+<li class="li_boot">ブーツ</li>
+</ul>
+<ul class="size_box">
+<li class="tops view size_firstview"><img src="https://dracaena-sv.sakura.ne.jp/dracaena/v4/images/size/tops.gif" alt="トップスサイズ見本"></li>
+<li class="bott"><img src="https://dracaena-sv.sakura.ne.jp/dracaena/v4/images/size/bott.gif" alt="ボトムスサイズ見本"></li>
+<li class="over"><img src="https://dracaena-sv.sakura.ne.jp/dracaena/v4/images/size/over.gif" alt="オーバーオールサイズ見本"></li>
+<li class="ponc"><img src="https://dracaena-sv.sakura.ne.jp/dracaena/v4/images/size/ponc.gif" alt="ポンチョサイズ見本"></li>
+<li class="ponc"><img src="https://dracaena-sv.sakura.ne.jp/dracaena/v4/images/size/snea.gif" alt="スニーカーサイズ見本"></li>
+<li class="ponc"><img src="https://dracaena-sv.sakura.ne.jp/dracaena/v4/images/size/boot.gif" alt="ブーツサイズ見本"></li>
+</ul>
+<p></p>
+
+<h3>お支払い方法</h3>
+<p class="use_info">
+<img src="https://dracaena-sv.sakura.ne.jp/dracaena/v4/images/use_info_card.jpg" alt="各種クレジットカード" class="float_left">
+商品のお支払方法は、クレジットカード・代金引換・銀行振込・コンビニ決済です。いずれかお好みの方法をお選びください。
+お支払い方法の手数料はクレジットカードが無料、代引引換が330円、コンビニ決済が220円です。銀行振込の場合はお使いの銀行の振込み手数料でお客様のご負担になります。
+</p>
+<h3>発送について</h3>
+<p class="use_info">
+営業日、昼14時までのご注文の方は当日発送可能ですのでお申し付けください。<br>
+銀行振込とコンビニ決済の方は、ご入金の確認が取れ次第の発送となります。着日希望が無い場合の商品到着目安は発送のご連絡後1～3日になります。メール便(着日希望不可)の場合は2～5日程度です。<br>
+【税込】10.000円以上お買い上げの場合は送料無料です。(沖縄やその他離島は700円)10,000円未満のお買い上げの場合は送料600円です。(沖縄やその他離島は1,300円)メール便をお選びの場合は全国一律320円です。
+</p>
+<h3>配送について</h3>
+<p class="use_info">
+配送方法はヤマト運輸(クロネコヤマト)宅急便・ネコポスとなります。Tシャツや薄手シャツ、バンダナ等薄い商品はメール便対象となります。 ※メール便に入らない商品をメール便でご注文頂いた場合は宅配便に切り替えさせて頂きます。その際、送料が発生する場合もございます事ご了承ください。
+<img src="https://dracaena-sv.sakura.ne.jp/dracaena/v4/images/use_info_posttime.jpg" alt="指定時間帯">
+ご希望の時間指定をお選び頂けます。指定なし（最短）・ 午前中（8～12時）・14～16時・16～18時・18～20時・19～21時の中からお選びください。
+</p>
+<h3>返品交換について</h3>
+<p class="use_info">
+当店の商品は全ていかなる理由でもご返品が可能です。
+商品到着後3日以内にご返品希望のメールかお電話をお願い致します。当店が理由の場合は送料は当店負担とさせていただきます。
+それ以外の理由の場合はお客様のご負担となります。
+ご返金は当店理由の場合は商品代全て、お客様都合の場合は「振込手数料」「弊社負担発送料」と別途「事務手数料」(消耗品費等含む)500円(税抜)を差し引いた金額とさせて頂きます。
+</p>
+<h3>お問合せ</h3>
+<p class="use_info">
+お問合せは <a href="mailto:info@e-dracaena.com">info@e-dracaena.com</a> にメールを頂くか、もしくは <a href="tel:0422-24-9456">0422-24-9456</a> までお電話ください。
+</p>
+</div>
+
